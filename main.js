360cafe3738d1fa3779ac6db691525d08f357b7b
Maryfs2805 <119754281+Maryfs2805@users.noreply.github.com>
2022-12-08 19:17:07 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+//alert("Bienvenidos a mi pagina");
+
+//Este código es para la animación
+let app = document.getElementById('typewriter');
+
+let typewriter = new Typewriter(app, {
+    loop: true,
+    delay: 75,
+});
+
+typewriter
+.pauseFor(2500)
+.typeString('Estudio en la carrera de Ingerieriá en Animación Digital y Efectos Visuales')
+.pauseFor(200)
+.deleteChars(10)
+.start();
